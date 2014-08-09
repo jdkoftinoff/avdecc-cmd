efd2b2e18559f0a13e7cf4dcdd1d7c760568bc98
Jeff Koftinoff <jeffk@jdkoftinoff.com>
2014-08-08 20:07:33 -0700

    Project avdecc-cmd started

---
+++ project.mk
@@ -0,0 +1,13 @@
+PROJECT=avdecc-cmd
+PROJECT_NAME=avdecc-cmd
+PROJECT_VERSION=20140808
+PROJECT_EMAIL=jeffk@jdkoftinoff.com
+PROJECT_LICENSE=private
+PROJECT_MAINTAINER=jeffk@jdkoftinoff.com
+PROJECT_COPYRIGHT=Copyright 2014 J.D. Koftinoff Software, Ltd.
+PROJECT_DESCRIPTION=avdecc-cmd
+PROJECT_WEBSITE=https://avb.statusbar.com/
+PROJECT_IDENTIFIER=com.statusbar.avb.avdecc-cmd
+TOP_LIB_DIRS+=.
+CONFIG_TOOLS+=
+PKGCONFIG_PACKAGES+=
