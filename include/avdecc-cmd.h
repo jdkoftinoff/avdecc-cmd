d66e8340cb7e00ee2847e6887668dd48817ca5cc
Jeff Koftinoff <jeffk@jdkoftinoff.com>
2014-08-08 20:29:33 -0700

    added get control. use underscores in filenames

--- include/avdecc-cmd.h
+++ include/avdecc-cmd.h
@@ -24,3 +24,14 @@ CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
 OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
+
+#include "jdksavdecc.h"
+
+
+#include "raw.h"
+
+#include "acmp.h"
+#include "adp.h"
+#include "set-control.h"
+#include "get-control.h"
+
